9ec4f6b054d0be8d761347dad6bc42ecc893fc67
FlutLab.io <>
2021-04-15 09:27:30 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ void main() {
     MaterialApp(theme: ThemeData(), debugShowCheckedModeBanner: false, home: HomePage()),
   );
 }
+
 class HomePage extends StatelessWidget {
+  Color primaryColor = Color(0xffedd3cb);
+
+  Color darkPrimaryColor = Color(0xff6f3d2e);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        
-
-    );
+    return Scaffold(backgroundcolor: primaryColor);
   }
 }
