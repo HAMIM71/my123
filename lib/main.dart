fe0e78c3b022f43059b6489d52e15543e90dc07b
FlutLab.io <>
2021-04-15 09:20:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
-// choose file
+import 'package:flutter/material.dart';
+import 'package:mydream/colors.dart'
+void main() {
+  runApp(
+    MaterialApp(theme: ThemeData(), debugShowCheckedModeBanner: false, home: HomePage()),
+  );
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: primaryColor
+
+    );
+  }
+}
