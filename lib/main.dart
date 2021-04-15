4303d3cfb0623749afc3d0bce776468674a94273
FlutLab.io <>
2021-04-15 09:32:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: primaryColor);
+    return Scaffold(backgroundColor: primaryColor,
+    body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          NavigationBar(),
+          Container(
+            margin: EdgeInsets.only(left: 40),
+            height: height / 2.5,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return AlbumArt();
+              },
+              itemCount: 1,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Text(
+            'Gidget',
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+                color: darkPrimaryColor),
+          ),
+          Text(
+            'The Free Nationals',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: darkPrimaryColor),
+          ), );
   }
 }
