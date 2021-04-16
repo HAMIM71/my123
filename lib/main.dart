88d52b61837d1cfd3b047b358becfb73471d9272
FlutLab.io <>
2021-04-16 04:27:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: primaryColor,
-   
-          ) )
+    return Scaffold(backgroundColor: primaryColor);
   }
 }
