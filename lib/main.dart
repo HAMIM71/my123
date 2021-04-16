1802529c81f2ba17612a805603533b1c575e32cc
HAMIM71 <71813345+HAMIM71@users.noreply.github.com>
2021-04-16 10:49:24 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
-// choose file
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(theme: ThemeData(), debugShowCheckedModeBanner: false, home: HomePage()),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  Color primaryColor = Color(0xffedd3cb);
+
+  Color darkPrimaryColor = Color(0xff6f3d2e);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: primaryColor,
+        body: Column(children: <widget>[
+          NavigationBar(),
+        ]));
+  }
+}
